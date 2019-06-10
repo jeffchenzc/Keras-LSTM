ec94664563b83941c5daeac595606c568311f4a0
Zhijie Chen <czjhk@Zhijies-MacBook-Pro.local>
2019-06-10 06:34:49 -0700

    model regression improvement by dropping dropout layers

--- LSTM_Electricity.py
+++ LSTM_Electricity.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 import re
 
 
-# In[75]:
+# In[167]:
 
 
-
+#This program predicts total power consumption of London in a day by examining past power consumptions and weather
 f = open("part-00000", "r")
 
 #retrive data from Spark output file
@@ -37,7 +37,6 @@ test_size = int(total * 0.3)
 inp = train_size + test_size
 ut_size = total - inp 
 ut_size = ut_size - (ut_size % 4)
-print(total, train_size, test_size, ut_size)
 
 train, test, ut = data[:train_size], data[train_size:inp], data[inp:inp+ut_size]
 x_test, y_test= test[:, 1:], test[:,0]
@@ -45,19 +44,16 @@ x_train, y_train = train[:, 1:], train[:, 0]
 x_ut, y_ut = ut[:, 1:], ut[:, 0]
 
 scale_size = 1000
-y_test, y_train, y_ut, output_shift=     np.divide(y_test, scale_size), np.divide(y_train, scale_size), np.divide(y_ut, scale_size),     np.divide(output_shift, scale_size)
+y_test, y_train, y_ut, output_shift=    np.divide(y_test, scale_size), np.divide(y_train, scale_size), np.divide(y_ut, scale_size),    np.divide(output_shift, scale_size)
+
 
 x_train = x_train.reshape((124,4,9))
 y_train = y_train.reshape((124,4,1))[:,2,:]
-#y_train = convertY(y_train)
 x_test = x_test.reshape((62,4,9))
 y_test = y_test.reshape((62,4,1))[:,2,:]
 
 x_ut = x_ut.reshape((20, 4, 9))
 y_ut = y_ut.reshape((20,4,1))[:,2,:]
-
-# y_test, y_train = tf.keras.utils.normalize(y_test, axis=1),\
-#     tf.keras.utils.normalize(y_train, axis=1)
     
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 x_train = tf.keras.utils.normalize(x_train, axis=1)
@@ -70,10 +66,6 @@ shift_test = shift_test.reshape(x_test.shape[0], 4, 1)
 shift_ut = output_shift[:,train_size+test_size:train_size+test_size+ut_size]
 shift_ut = shift_ut.reshape(x_ut.shape[0], 4, 1)
 
-# shift_test, shift_train, shift_ut = \
-#     tf.keras.utils.normalize(shift_test, axis=1),\
-#     tf.keras.utils.normalize(shift_train, axis=1),\
-#     tf.keras.utils.normalize(shift_ut, axis=1)
 
 x_train = np.dstack((shift_train, x_train))
 x_test = np.dstack((shift_test, x_test))
@@ -81,31 +73,26 @@ x_ut = np.dstack((shift_ut, x_ut))
 
 #building LSTM model
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.LSTM(1000, return_sequences=False, input_shape=(4, 10), dropout=0.2))
-model.add(tf.keras.layers.Dense(100))
-model.add(tf.keras.layers.Dropout(0.1))
+model.add(tf.keras.layers.LSTM(80, return_sequences=False, input_shape=(4, 10)))
+model.add(tf.keras.layers.Dense(20))
 model.add(tf.keras.layers.Dense(1))
 model.compile(loss='mae', optimizer='adam')
 model.fit(x_train, y_train, epochs=90, validation_data=(x_test, y_test))
 
 
-# In[80]:
+# In[168]:
 
 
-# xreshape = x_test[14].reshape((1,3,9))
-# yreshape = y_test[14].reshape((1,1))
+#print loss of the last 10% unseen data and plot prediction(blue) against actual values(red)
+#note: Unit of y axis is 1k
 pred = model.predict(x_ut)
-pred += 10
-# pred = pred[:,2,:]
-# pred = pred.reshape((pred.shape[0], 1))
 plt.plot(pred, color="blue")
 
 against = y_ut
-# against = against[:,2,:]
-# against = against.reshape((against.shape[0], 1))
 plt.plot(against, color="red")
-#plt.plot(y_test)
-#print(loss, acc)
+
+vloss = model.evaluate(x_ut, y_ut)
+print(vloss)
 
 
 # In[ ]:
